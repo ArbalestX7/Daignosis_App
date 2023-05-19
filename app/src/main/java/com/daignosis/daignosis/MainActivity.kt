eaef0bf572cb92ff9dcd16c49759df8cd4e287b6
Dito <ditoiqbalk@gmail.com>
2023-05-19 08:13:04 +0700

    add Statis UI on Homepage and add plugin

--- app/src/main/java/com/daignosis/daignosis/MainActivity.kt
+++ app/src/main/java/com/daignosis/daignosis/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.daignosis.daignosis
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import com.daignosis.daignosis.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setupView()
+        supportActionBar?.hide()
+
+
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
     }
 }
